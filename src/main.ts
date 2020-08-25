744ec168c576c1def1beca6532a5877710a720e7
unknown <huangjinkai@auxgroup.com>
2020-08-25 14:47:31 +0800

    加入导航栏

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import store from './store'
+
 Vue.config.productionTip = false
 
-new App().$mount()
+Vue.prototype.$store = store
+
+// App.mpType = 'app'
+
+const app = new App({
+	store
+})
+app.$mount()
